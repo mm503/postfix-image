0f77d50a1348cf0f31b6ee1767c393e053dd7a8b
Michal Middleton <jafa81@gmail.com>
2023-12-28 18:59:34 -0600

    Fix postalias

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,7 @@ if [ "$1" = 'postfix' ]; then
   # postfix needs fresh copies of files in its chroot jail
   cp /etc/{hosts,localtime,nsswitch.conf,resolv.conf,services} /var/spool/postfix/etc/
 
-  postmap /etc/aliases
+  postalias /etc/aliases
   postmap /etc/postfix/generic
   postmap /etc/postfix/virtual
 
